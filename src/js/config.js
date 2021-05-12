b68ae53e946c87efa87136498c6c10dba0b6600f
kravetsdesign <kravetsdesign@gmail.com>
2021-05-12 13:42:43 -0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b59202bf-5ec0-4e20-9bd0-391b53d29557';
+export const MODAL_CLOSE_SEC = 2;
